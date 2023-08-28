eb178bc9b9d7fde9321d739089744db1ef79749c
CheshireCatNick <CheshireCatNick@gmail.com>
2023-08-28 15:10:23 +0000

    chore: add aimbot

--- src/market.ts
+++ src/market.ts
@@ -43,6 +43,7 @@ export const marketNameToPage: { [key: string]: string } = {
     RETH: 'rocket-pool-eth',
     AICODE: 'ai-code',
     SFRXETH: 'frax-staked-ether',
+    AIMBOT: 'aimbot',
     '2330': '2330.TW',
     '2454': '2454.TW',
     '0050': '0050.TW',
