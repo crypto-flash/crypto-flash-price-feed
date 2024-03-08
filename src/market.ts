6051fea4778eebc666fc5bb26d8a0205f0b00271
CheshireCatNick <CheshireCatNick@gmail.com>
2024-03-08 23:12:41 +0800

    chore: add JUP

--- src/market.ts
+++ src/market.ts
@@ -45,6 +45,7 @@ export const marketNameToPage: { [key: string]: string } = {
     VRTX: 'vertex-protocol',
     TIA: 'celestia',
     ALT: 'altlayer',
+    JUP: 'jupiter-ag',
     '2330': '2330.TW',
     '2454': '2454.TW',
     '0050': '0050.TW',
