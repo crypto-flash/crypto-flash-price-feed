1e9dd61a79e60129ce5787d20505606659dfaee6
CheshireCatNick <CheshireCatNick@gmail.com>
2024-09-15 11:37:11 +0800

    binance throw error instead of undefined

--- src/price-source/binance-price-source.test.ts
+++ src/price-source/binance-price-source.test.ts
@@ -2,7 +2,7 @@ import { BinancePriceSource } from './binance-price-source'
 
 test('binance price source', async () => {
     const binancePriceSource = new BinancePriceSource()
-    const price = await binancePriceSource.fetch('BTC')
+    const price = await binancePriceSource.fetch('BTCUSDT')
 
     expect(price).not.toBeUndefined
     expect(+price!).toBeGreaterThan(0)
