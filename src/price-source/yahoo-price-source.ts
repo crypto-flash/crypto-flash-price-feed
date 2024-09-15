f9e11cb1f257ceb4f627992f8a36c81655615f61
CheshireCatNick <CheshireCatNick@gmail.com>
2024-09-15 11:44:38 +0800

    yahoo is broken

--- src/price-source/yahoo-price-source.ts
+++ src/price-source/yahoo-price-source.ts
@@ -1,27 +1,19 @@
 import { PriceSource } from './price-source'
 import Big from 'big.js'
-import { log } from '../helper'
 import YahooStockAPI from 'yahoo-stock-api'
 import { getSymbolResponse } from 'yahoo-stock-api/dist/types/getSymbol'
-import { marketNameToPage } from '../market'
 
 export class YahooPriceSource implements PriceSource {
     private readonly client = new YahooStockAPI()
 
     async fetch(symbol: string) {
-        if (!(symbol in marketNameToPage)) {
-            return undefined
+        /* FIXME
+        const result = await this.client.getSymbol({ symbol })
+        if (result.error) {
+            throw new Error(`${result.message}`)
         }
-        try {
-            const result = await this.client.getSymbol({ symbol: marketNameToPage[symbol] })
-            if (result.error) {
-                throw new Error(`${result.message}`)
-            }
-            // use bid as the current sell price
-            return Big((result.response as unknown as getSymbolResponse).bid.value)
-        } catch (err: any) {
-            log(`[YahooPriceSource] fetch price for ${symbol} error: ${err}`)
-            return undefined
-        }
+        // use bid as the current sell price
+        return Big((result.response as unknown as getSymbolResponse).bid.value)*/
+        return Big(0)
     }
 }
