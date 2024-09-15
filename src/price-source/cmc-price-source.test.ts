5aa9112307a506702833f5838c4e79799b791bf5
CheshireCatNick <CheshireCatNick@gmail.com>
2024-09-15 11:39:05 +0800

    CMC throw error instead of undefined

--- src/price-source/cmc-price-source.test.ts
+++ src/price-source/cmc-price-source.test.ts
@@ -2,7 +2,7 @@ import { CMCPriceSource } from './cmc-price-source'
 
 test('cmc price source', async () => {
     const cmcPriceSource = new CMCPriceSource()
-    const price = await cmcPriceSource.fetch('BTC')
+    const price = await cmcPriceSource.fetch('bitcoin')
 
     expect(price).not.toBeUndefined
     expect(+price!).toBeGreaterThan(0)
